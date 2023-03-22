e0e008fbd4f233bc7e0fe5a9c9e006aad0fb6bbe
KazakSlava <114358733+KazakSlava@users.noreply.github.com>
2023-03-22 13:12:41 +0200

    optate file
    optate file

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+const bodyEl = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+ let getId = null;
+
+btnStart.addEventListener("click", startChangeBgColor);
+btnStop.addEventListener("click", stopChangeBgColor);
+
+function startChangeBgColor() {
+  getId = setInterval(() => bodyEl.style.backgroundColor = getRandomHexColor(), 1000);
+  btnStart.disabled  = true;
+  btnStop.disabled  = false;
+}
+
+function stopChangeBgColor() {
+    clearInterval(getId);
+    btnStart.disabled  = false;
+    btnStop.disabled  = true;
+  }
+
+function stopChangeBgColor() {
+    clearInterval(getId);
+    btnStart.disabled  = false;
+    btnStop.disabled  = true;
+  }
